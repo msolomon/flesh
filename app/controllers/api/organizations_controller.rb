0d12730b2e65dafa4b8194c11b4e0063e57fa58e
Mike Solomon <mike@mindsnacks.com>
2014-01-26 13:37:35 -0800

    Organization show method

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -1,11 +1,21 @@
 class Api::OrganizationsController < ApplicationController
   def index
-    @organizations = Organization.includes(:games, :users).map { |organization|
+    @organizations = Organization.includes(:games, :users).map &inflate
+    respond_with(@organizations)
+  end
+
+  def show
+    @organization = inflate.call Organization.includes(:games, :users).find params[:id]
+    respond_with(@organization)
+  end
+
+private
+  def inflate
+    lambda { |organization|
       response = organization.attributes
       response[:games] = organization.games.collect(&:id)
       response[:users] = organization.users.collect(&:id)
       response
     }
-    respond_with(@organizations)
   end
 end
