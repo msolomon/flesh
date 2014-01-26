f9734e7063ac8a730c98ed219d3c03c02c554144
Mike Solomon <mike@mindsnacks.com>
2014-01-26 14:01:08 -0800

    Player GETs

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,2 +1,20 @@
-class GamesController < InheritedResources::Base
+class Api::GamesController < InheritedResources::Base
+  def index
+    @games = Game.includes(:players).map &inflate
+    respond_with(@games)
+  end
+
+  def show
+    @game = inflate.call Game.includes(:players).find params[:id]
+    respond_with(@game)
+  end
+
+private
+  def inflate
+    lambda { |game|
+      response = game.attributes
+      response[:player_ids] = game.players.collect(&:id)
+      response
+    }
+  end
 end
