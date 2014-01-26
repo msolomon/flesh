3f12b8a484911839e06f700d6434d3ff8789e946
Mike Solomon <mike@mindsnacks.com>
2014-01-26 10:59:29 -0800

    Include games IDs with organization

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -1,5 +1,10 @@
 class Api::OrganizationsController < ApplicationController
   def index
-    respond_with(@organizations = Organization.all)
+    @organizations = Organization.includes(:game).map { |organization_model|
+      organization = organization_model.attributes
+      organization[:games] = organization_model.game.collect(&:id)
+      organization
+    }
+    respond_with(@organizations)
   end
 end
