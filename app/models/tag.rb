da2794a1f0782e4735f81c867237dc0462884f79
Mike Solomon <mike@mindsnacks.com>
2014-03-01 16:18:18 -0800

    Added tag events

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,17 +1,44 @@
 class Tag < ActiveRecord::Base
   include ModelMixin
+
+  after_create :record_tag_event
   
   belongs_to :tagger, class_name: 'Player'
   belongs_to :taggee, class_name: 'Player'
 
   has_one :event_link, as: :eventable
   has_one :event, through: :event_link
+  has_one :game, through: :tagger
 
   validates :claimed, presence: true
   validate :tagger_is_not_taggee
 
   def tagger_is_not_taggee
     errors[:base] << "You cannot tag yourself" if tagger == taggee
+  end
+
+  def game
+    read_attribute(:game) || self.taggee.nil? ? nil : self.taggee.game
+  end
+
+private
+
+  def record_tag_event
+    event = Event.create(event_type: :tag, data: {
+      tag_id: self.id,
+    })
+
+    self.event = event
+    self.game.events << event
+    self.game.organization.events << event
+    unless tagger.nil?
+      self.tagger.events << event
+      self.tagger.user.events << event
+    end
+    unless taggee.nil?
+      self.taggee.user.events << event
+      self.taggee.events << event
+    end
   end
 
 end
