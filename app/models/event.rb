d89eb4c934d370b895cca9ce714dd19d427a4720
Mike Solomon <mike@mindsnacks.com>
2014-03-01 17:11:24 -0800

    Added hstore accessors for event fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,6 +10,13 @@ class Event < ActiveRecord::Base
     :join_game,
     :tag
   ]
-  
+
+  hstore_accessor :data,
+                  tag_id: :integer,
+                  user_id: :integer,
+                  game_id: :integer,
+                  organization_id: :integer,
+                  player_id: :integer
+
   has_many :event_links
 end
