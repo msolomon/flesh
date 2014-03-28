bab3f1e67ffed3cac9bafba63f9182b8fc14eb76
Mike Solomon <mike@mindsnacks.com>
2014-03-28 00:02:09 -0700

    Bigger gravatars

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -19,7 +19,7 @@ class UserSerializer < ActiveModel::Serializer
 
   def avatar_url
     email_hash = Digest::MD5.hexdigest(object.email.downcase)
-    "https://www.gravatar.com/avatar/#{email_hash}?d=retro"
+    "https://www.gravatar.com/avatar/#{email_hash}?d=retro&s=200"
   end
 
   def include_email?
