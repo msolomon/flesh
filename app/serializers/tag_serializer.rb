0a475dcc63e6ecbacd1fd44947c22cfd4f4432fc
Mike Solomon <mike@mindsnacks.com>
2014-03-30 12:45:10 -0700

    More robust OZ shadow player

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,5 +1,7 @@
 class TagSerializer < ActiveModel::Serializer
   include SerializerMixin
+  
+  OZ = "Original Zombie"
 
   embed :ids
 
@@ -11,7 +13,7 @@ class TagSerializer < ActiveModel::Serializer
   def tagger
     tagger_player = object.tagger
     if !is_me?(tagger_player.user) && tagger_player.is_stealthed?
-      Player.new(id: 0)
+      Player.new(id: 0, oz_status: :confirmed, user: User.new(screen_name: OZ, first_name: OZ, last_name: OZ, email: "flesh.io@monumentmail.com"))
     else
       tagger_player
     end
