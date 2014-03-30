dc16d39f77045600fbc80fa39fdbe86e5b7b1744
Mike Solomon <mike@mindsnacks.com>
2014-03-30 11:37:34 -0700

    Hide OZ tags on Player/User feeds while stealthed

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -5,21 +5,33 @@ class Api::EventsController < Api::ApiController
   DEFAULT_LIMIT = 20
   DEFAULT_OFFSET = 0
 
-  EVENTABLE_MAP = {
-    game: Game,
-    user: User,
-    player: Player,
-    organization: Organization
-  }
-
   def events
-    respond_with_events(EVENTABLE_MAP[params[:resource]])
+    # TODO: write tests for this, I only did quick ad-hoc tests
+    case params[:resource]
+    when :organization
+      respond_with_events Organization.find(params[:id]).events
+    when :game
+      respond_with_events Game.find(params[:id]).events
+    when :user
+      secret_events = User.find(params[:id]).players.flat_map {|player| get_secret_events_for_player player}.to_set
+      respond_with_events player.events.where.not(secret_events)
+    when :player
+      secret_events = get_secret_events_for_player(Player.find(params[:id]))
+      respond_with_events player.events.where.not(secret_events)
   end
   
 private
 
-  def respond_with_events model
-    respond_with(model.find(params[:id]).events.limit(params[:limit]).offset(params[:offset]))
+  def respond_with_events events
+    respond_with(events.limit(params[:limit]).offset(params[:offset]))
+  end
+
+  def get_secret_events_for_player player
+    if player.is_stealthed?
+      secret_events = Event.where(event_type: Event.event_types[:tag]).where("data -> 'tag_id' = ANY('{?}')", player.tags.pluck(:id))
+    else
+      Event.none
+    end
   end
 
   def preprocess_params
