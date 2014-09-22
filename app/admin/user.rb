9f3dbfb5baf71bc79dc2be80aa3e88d655adc935
Mike Solomon <msolomon@twitter.com>
2014-09-21 18:16:15 -0700

    Edit users in admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,8 +1,11 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :admin, :first_name, :last_name, :screen_name
 
   index do
     column :admin
+    column :screen_name
+    column :first_name
+    column :last_name
     column :email
     column :current_sign_in_at
     column :last_sign_in_at
@@ -13,16 +16,17 @@ ActiveAdmin.register User do
   filter :email
 
   controller do
-    def update
-      if params[:user][:password].blank?
-        params[:user].delete("password")
-      end
-      super
+    def update_resource(object, attributes)
+      update_method = attributes.first[:password].present? ? :update_attributes : :update_without_password
+      object.send(update_method, *attributes)
     end
   end
 
   form do |f|
     f.inputs "Admin Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :screen_name
       f.input :email
       f.input :admin
       f.input :password
