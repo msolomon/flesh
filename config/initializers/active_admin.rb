1a2ece57b3e073055ff13ef124f6d9a2ee066c36
Mike Solomon <msolomon@twitter.com>
2014-09-21 17:14:16 -0700

    ActiveAdmin 403 instead of redirect

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -7,6 +7,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Flesh"
 
+  config.on_unauthorized_access = :render_403
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
